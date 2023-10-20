84f42bf3b733f3dd3d1a151763ec4a70236bdcb5
kleyfiex <kleyfie92@gmail.com>
2023-10-20 21:34:45 +0300

    test

--- JS/script.js
+++ JS/script.js
@@ -1,43 +1,43 @@
-let latitude, longitude;
-
 if ("geolocation" in navigator) {
     navigator.geolocation.getCurrentPosition(function(position) {
         // Координаты пользователя доступны в объекте `position`
-        latitude = position.coords.latitude;
-        longitude = position.coords.longitude;
-    
-      }, function(error) {
+        let userLocation = {
+            lat: position.coords.latitude,
+            lng: position.coords.longitude
+        };
+
+        const api = '792c80c8ee642d0e8438345141e93194';
+        const url = `https://api.openweathermap.org/data/2.5/onecall?lat=${userLocation.lat}&lon=${userLocation.lng}&exclude=current&appid=${api}`;
+        console.log(userLocation);
+
+        fetch(url)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json(); // Парсим JSON ответ, если он имеется
+            })
+        .then(data => {
+            // Обрабатываем полученные данные
+            console.log(data);
+            })
+        .catch(error => {
+            // Обрабатываем ошибку
+            console.error('There was a problem with the fetch operation:', error);
+        });   
+        }, function(error) {
         // Обработка ошибки
         switch (error.code) {
-          case error.PERMISSION_DENIED:
-            alert("Без местоположения вы не узнаете погоду");
+            case error.PERMISSION_DENIED:
+                alert("Без местоположения вы не узнаете погоду");
             break;
-          case error.POSITION_UNAVAILABLE:
-            alert("Информация о вашем местоположении недоступна");
+            case error.POSITION_UNAVAILABLE:
+                alert("Информация о вашем местоположении недоступна");
             break;
         }
-      });
+        });
   } else {
     alert("Геолокация не поддерживается браузером");
   }
-  
-console.log(latitude + " " + longitude);
 
 
-
-
-/* fetch('https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude=current&appid=c352dcf016bfd35b7eec4ac20510f7e4')
-  .then(response => {
-    if (!response.ok) {
-      throw new Error('Network response was not ok');
-    }
-    return response.json(); // Парсим JSON ответ, если он имеется
-  })
-  .then(data => {
-    // Обрабатываем полученные данные
-    console.log(data);
-  })
-  .catch(error => {
-    // Обрабатываем ошибку
-    console.error('There was a problem with the fetch operation:', error);
-  }); */
