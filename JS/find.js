78f9d6ffe165a5b3978fad6012f9dbb4e9c16271
kleyfiex <kleyfie92@gmail.com>
2023-10-29 14:14:58 +0300

    weather-end

--- JS/find.js
+++ JS/find.js
@@ -1,13 +1,26 @@
 document.addEventListener('DOMContentLoaded', function () {
     const btnFind = document.querySelector('.btn-find');
+    const cityInput = document.querySelector('.field-find');
 
     btnFind.onclick = function() {
         // Получаем введенный текст из инпута
-        const cityInput = document.querySelector('.field-find');
         const city = cityInput.value.trim();
 
         // Сохраняем введенный город в localStorage
         localStorage.setItem('selectedCity', city);
 
     };
+
+    cityInput.addEventListener("keydown", (e) => {
+        if(e.keyCode === 13) {
+            // Получаем введенный текст из инпута
+            const city = cityInput.value.trim();
+
+            // Сохраняем введенный город в localStorage
+            localStorage.setItem('selectedCity', city);
+            window.location.href = "index.html";
+        }
+    })
+
+
 })
--- JS/index.js
+++ JS/index.js
@@ -1,20 +1,34 @@
 
 window.addEventListener('beforeunload', function (event) {
-    localStorage.removeItem('selectedCity');
+    localStorage.removeItem('selectedCity'); // очищаем ввод города вручную, если была перезагрузка страницы
 });
 
 const weatherContent = document.querySelector('.weather-content');
-const selectedCity = localStorage.getItem('selectedCity');
+const selectedCity = localStorage.getItem('selectedCity'); // получаем введенный город
 const api = '4379e359a43e158fba51dfd6ab6c79f2';
+const apiIp = 'at_ulnQMrTr4JdZQvyY4kDLKaICtLJZR';
+const errorContainer = document.querySelector('errorContainer');
 
 
-if (selectedCity) {
+if (selectedCity) { // если переменная города не пустая, то получаем координаты введенного города
   getCoordsFromCity(selectedCity);
 }
 
-
-// Автоматическое определение местоположения и погоды пользователя
-navigator.geolocation.getCurrentPosition(geoCorrect, geoFail);
+// Проверяем, была ли геолокация уже запрошена в текущей сессии
+if (!sessionStorage.getItem('geolocationRequested')) {
+    // Запрашиваем геолокацию
+    navigator.geolocation.getCurrentPosition(geoCorrect, geoFail);
+  
+    // Помечаем, что геолокация была запрошена в текущей сессии
+    sessionStorage.setItem('geolocationRequested', 'true');
+  }
+  
+  
+function getCookie(name) {
+    var value = "; " + document.cookie;
+    var parts = value.split("; " + name + "=");
+    if (parts.length === 2) return parts.pop().split(";").shift();
+  }
 
 // Если пользователь разрешил доступ к геолокации
 function geoCorrect(position) {
@@ -23,26 +37,26 @@ function geoCorrect(position) {
 
 // Если пользователь запретил доступ к геолокации
 function geoFail(positionError) {
-    // fetch('https://api.ipify.org?format=json')
-    //     .then((response) => {
-    //         return response.json()
-    //     })
-    //     .then((data) => {
-    //         getCityFromIp(data.ip)
-    //     })
-    //     .catch(error => {
-    //         console.error("Произошла ошибка при определении погоды: ")
-    //         showErrorContainer()
-    //     })
+    fetch('https://api.ipify.org?format=json')
+        .then((response) => { 
+             return response.json() // получаем json
+        })
+         .then((data) => { // работаем с данными
+            getIpToCity(data.ip)
+         })
+         .catch(error => { // ловим ошибку если она появится
+            console.error("Произошла ошибка при определении погоды: " + error.message)
+            showError();
+        })
 }
 
 // Функция определения погоды по координатам
 function getWeatherFromCoords(lat, lon, currentCity) {
-  fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=bd69def61e044b12aa285f853e73965f`)
+  fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${api}`)
       .then((response) => {
           return response.json()
       })
-      .then((data) => {
+      .then((data) => { 
           console.log(data)
           
           let city;
@@ -50,29 +64,33 @@ function getWeatherFromCoords(lat, lon, currentCity) {
               city = data.name
           }
           else {
-              city = selectedCity
+              city = currentCity
           }
+        
+        let weatherDescription = data.weather[0].description;
+        // Преобразование первой буквы описания погоды в заглавную
+        weatherDescription = weatherDescription[0].toUpperCase() + weatherDescription.slice(1);
 
-          const weatherContent = document.createElement('div');
-      weatherContent.classList.add('weather-content');
+        const weatherContent = document.createElement('div');
+        weatherContent.classList.add('weather-content');
 
-      const degrees = document.createElement('span');
-      degrees.classList.add('degrees');
-      degrees.textContent = `${data.main.temp}℃`;
+        const degrees = document.createElement('span');
+        degrees.classList.add('degrees');
+        degrees.textContent = `${data.main.temp}℃`;
 
-      const cityText = document.createElement('p');
-      cityText.classList.add('city');
-      cityText.textContent = `Windy in ${city}`;
+        const cityText = document.createElement('p');
+        cityText.classList.add('city');
+        cityText.textContent = `${weatherDescription} in ${city}`;
 
-      const changeCityLink = document.createElement('a');
-      changeCityLink.classList.add('change_city');
-      changeCityLink.href = 'find.html';
-      changeCityLink.textContent = 'Change city';
+        const changeCityLink = document.createElement('a');
+        changeCityLink.classList.add('change_city');
+        changeCityLink.href = 'find.html';
+        changeCityLink.textContent = 'Change city';
 
-      // Добавляем созданные элементы в блок .weather-content
-      weatherContent.appendChild(degrees);
-      weatherContent.appendChild(cityText);
-      weatherContent.appendChild(changeCityLink);
+        // Добавляем созданные элементы в блок .weather-content
+        weatherContent.appendChild(degrees);
+        weatherContent.appendChild(cityText);
+        weatherContent.appendChild(changeCityLink);
 
       // Находим блок, который нужно заменить
       const currentWeather = document.querySelector('.weather-content');
@@ -81,42 +99,29 @@ function getWeatherFromCoords(lat, lon, currentCity) {
       currentWeather.parentNode.replaceChild(weatherContent, currentWeather);
       })
       .catch(error => {
-          console.error("Произошла ошибка при определении погоды: " + error.message)
+          console.error("Произошла ошибка при определении погоды: " + error.message);
+          showError();
       })
 }
 
-function createHtmlElements(dataWeather, curCity) {
-      const weatherContent = document.createElement('div');
-      weatherContent.classList.add('weather-content');
-
-      const degrees = document.createElement('span');
-      degrees.classList.add('degrees');
-      degrees.textContent = `${dataWeather.main.temp}℃`;
-
-      const cityText = document.createElement('p');
-      cityText.classList.add('city');
-      cityText.textContent = `Windy in ${curCity}`;
-
-      const changeCityLink = document.createElement('a');
-      changeCityLink.classList.add('change_city');
-      changeCityLink.href = 'find.html';
-      changeCityLink.textContent = 'Change city';
-
-      addHtmlElements(weatherContent, degrees, cityText, changeCityLink);
+// Функция определения координат города по ip
+function getIpToCity(ip) {
+    fetch(`https://geo.ipify.org/api/v2/country,city?apiKey=${apiIp}&ipAddress=${ip}`)
+        .then((response) => {
+            return response.json() // получаем json
+        })
+        .then((data) => { // работаем с данными
+            console.log(data)
+            let locationLat = data.location.lat;
+            let locationLng = data.location.lng;
+            getWeatherFromCoords(locationLat, locationLng, null)
+        })
+        .catch(error => {
+            console.error("Произошла ошибка при определении города: " + error.message)
+            showError()
+        })
 }
 
-function addHtmlElements(weather, degree, city, change) {
-      // Добавляем созданные элементы в блок .weather-content
-      weather.appendChild(degree);
-      weather.appendChild(city);
-      weather.appendChild(change);
-
-      // Находим блок, который нужно заменить
-      const currentWeather = document.querySelector('.weather-content');
-
-      // Заменяем блок новым содержанием
-      currentWeather.parentNode.replaceChild(weatherContent, currentWeather);
-}
 
 // Функция определения координат города
 function getCoordsFromCity(city) {
@@ -126,17 +131,19 @@ function getCoordsFromCity(city) {
       })
       .then((data) => {
           console.log(data)
-
           // Получение координат из Json
           let lat = data[0].lat
           let lon = data[0].lon
           let cityName = data[0].name
-
           // Определение погоды по координатам
           getWeatherFromCoords(lat, lon, cityName)
       })
       .catch(error => {
-          console.error("Произошла ошибка при определении местоположения города: " + error.message)
-          showErrorContainer()
+          console.error("Произошла ошибка при определении местоположения города: " + error.message);
+          showError();
       })
+}
+
+function showError() {
+    window.location.href = "fail.html";
 }
